118ef8476392e510853d8ecefd3b156f239978ab
Victor Barros <victorwbarros@gmail.com>
2022-09-19 17:56:32 -0300

    fix insert query on migration

--- prisma/migrations/20220918223625_change_column_name_and_pdfurl_from_test_to_unique/migration.sql
+++ prisma/migrations/20220918223625_change_column_name_and_pdfurl_from_test_to_unique/migration.sql
@@ -14,7 +14,6 @@ DROP INDEX "test_pdfUrl_key";
 CREATE UNIQUE INDEX "test_name_pdfUrl_teacherDisciplinesId_key" ON "test"("name", "pdfUrl", "teacherDisciplinesId");
 
 
-// módulos do curso
 INSERT INTO terms ("number") VALUES (1);
 INSERT INTO terms ("number") VALUES (2);
 INSERT INTO terms ("number") VALUES (3);
@@ -22,16 +21,13 @@ INSERT INTO terms ("number") VALUES (4);
 INSERT INTO terms ("number") VALUES (5);
 INSERT INTO terms ("number") VALUES (6);
 
-// tipos de provas
 INSERT INTO categories ("name") VALUES ('Projeto');
 INSERT INTO categories ("name") VALUES ('Prática');
 INSERT INTO categories ("name") VALUES ('Recuperação');
 
-// professores(as)
 INSERT INTO teachers ("name") VALUES ('Diego Pinho');
 INSERT INTO teachers ("name") VALUES ('Bruna Hamori');
 
-// disciplinas
 INSERT INTO disciplines ("name", "termId") VALUES ('HTML e CSS', 1);
 INSERT INTO disciplines ("name", "termId") VALUES ('JavaScript', 2);
 INSERT INTO disciplines ("name", "termId") VALUES ('React', 3);
@@ -39,7 +35,6 @@ INSERT INTO disciplines ("name", "termId") VALUES ('Humildade', 1);
 INSERT INTO disciplines ("name", "termId") VALUES ('Planejamento', 2);
 INSERT INTO disciplines ("name", "termId") VALUES ('Autoconfiança', 3);
 
-// professores(as) e disciplinas
 INSERT INTO "teachersDisciplines" ("teacherId", "disciplineId") VALUES (1, 1);
 INSERT INTO "teachersDisciplines" ("teacherId", "disciplineId") VALUES (1, 2);
 INSERT INTO "teachersDisciplines" ("teacherId", "disciplineId") VALUES (1, 3); 
